840eed257526d506b5f813145e50b85b84aaf7bb
stefanuzelac <stefanuzelac1010@gmail.com>
2023-12-23 17:37:40 +0100

    Miniscule changes before Exercises overhaul.

--- app/src/main/java/com/example/fitnessapp2/api/ExerciseApi.java
+++ app/src/main/java/com/example/fitnessapp2/api/ExerciseApi.java
@@ -2,7 +2,6 @@ package com.example.fitnessapp2.api;
 
 import com.example.fitnessapp2.adapters.ExerciseCallback;
 
-//retrieving exercises from an API
 public interface ExerciseApi {
     void getExercises(String muscle, ExerciseCallback callback);
 
